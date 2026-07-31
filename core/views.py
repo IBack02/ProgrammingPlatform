d03f7ca27025e53f655e890179f59d9987551f46
Admin <aibek@mail.com>
2026-08-01 02:47:04 +0500

    Image resolutions are fixed, overlay is removed

--- core/views.py
+++ core/views.py
@@ -248,7 +248,7 @@ def _require_theory_quiz_owner_or_404(module_id: int, teacher: Teacher):
 
 def _student_background_urls() -> dict:
     # A single Drive image is currently used for all student-facing backgrounds.
-    background_url = "https://drive.google.com/thumbnail?id=1hzxqoXk4FBFtbJg000np3vYArAZwlXHZ&sz=w2200"
+    background_url = "https://drive.google.com/thumbnail?id=1hzxqoXk4FBFtbJg000np3vYArAZwlXHZ&sz=w6000"
     return {
         "dashboard": background_url,
         "coding": background_url,
